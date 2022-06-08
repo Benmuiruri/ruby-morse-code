9bc1f28431892109917327b0cab5b7ea9bef7f24
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-08 16:39:23 +0300

    Fix Rubocop offenses

--- morse_code.rb
+++ morse_code.rb
@@ -25,25 +25,25 @@ MORSE_CODE = {
   '-..-' => 'X',
   '-.--' => 'Y',
   '--..' => 'Z'
-}
+}.freeze
 
 def decode_char(char)
   MORSE_CODE[char]
 end
 
 def decode_word(str)
-  str.split(" ").map {
-    |character| decode_char(character)
-  }.join("")
+  str.split.map do |character|
+    decode_char(character)
+  end.join
 end
 
 def decode_message(long_str)
-  long_str.strip.split("   ").map {
-    |word| decode_word(word)
-  }.join(" ")
+  long_str.strip.split('   ').map do |word|
+    decode_word(word)
+  end.join(' ')
 end
 
-puts decode_message("      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 
 # This is a more compressed shorter version of the above
 # def decode_morse(morse_code)
