c0a2a2d3025a6fc10b3eb2e85ce887fde84fdf1d
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-09 10:59:03 +0300

    Create morse_code dictionary and create method to decode character

--- morse_code.rb
+++ morse_code.rb
@@ -30,28 +30,3 @@ MORSE_CODE = {
 def decode_char(char)
   MORSE_CODE[char]
 end
-
-def decode_word(str)
-  str.split.map do |character|
-    decode_char(character)
-  end.join
-end
-
-def decode_message(long_str)
-  long_str.strip.split('   ').map do |word|
-    decode_word(word)
-  end.join(' ')
-end
-
-puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
-# This is a more compressed shorter version of the above
-# def decode_morse(morse_code)
-#   morse_code
-#     .split('   ')
-#     .map do |word|
-#       word.split(' ').map { |char| MORSE_CODE[char] }.join
-#     end
-#     .join(' ')
-# end
-# puts decode_morse("-- -.--   -. .- -- .")
